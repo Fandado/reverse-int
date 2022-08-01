c2002c8e759584a3f84eba32b8f885c22d290f33
Fandado <a.pyatetskaya.nastya@gmail.com>
2022-08-01 23:18:01 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n);
+    let result = ('' + number).split('').reverse().join();
+    result = result.replace(/,/g, '');
+    return result
 }
